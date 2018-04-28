490cf7223193c4bf800baaa60551e88f698cd609
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-04-28 19:38:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Nick"
-var height = "68"
+var height = "67"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height+1)
+console.log(parseInt(height)+1)
